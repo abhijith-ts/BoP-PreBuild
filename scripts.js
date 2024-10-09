a81dd5d080870b965fdd68a2a57772d696777b51
Pranav M <pranavm265@gmail.com>
2024-10-10 01:40:20 +0530

    js added; api 1 & 2

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.getElementById('api1button').addEventListener('click', async () => {
+    await fetch('https://official-joke-api.appspot.com/random_joke/')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('api1displayer').innerText = JSON.stringify(data.setup,null,2)
+            document.getElementById('api1follower').textContent = JSON.stringify(data.punchline,null,2)          
+            console.log(data)
+        })
+})
+
+document.getElementById('api2button').addEventListener('click', async () => {
+    await fetch('https://meme-api.com/gimme')
+        .then(response => response.json())
+        .then(data => {
+            len = data.preview.length
+            document.getElementById('api2displayer').innerHTML = null
+            document.getElementById('random-image').src = data.preview[len - 1];
+            console.log(data)
+        })
+})
